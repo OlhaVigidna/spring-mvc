b00afd69627420ec28174622d69cd79fdd3a9139
olga <olya.vigidna@gmail.com>
2019-06-10 16:02:58 +0300

    main commit

---
+++ src/main/java/com/example/springmvc/service/UserService.java
@@ -0,0 +1,9 @@
+package com.example.springmvc.service;
+
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+//    void findByName
+}
